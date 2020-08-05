ae2258d701ea96fc6f63a92c039285adeea01d32
Cathryn <lifamily4@hotmail.com>
2020-08-04 21:27:52 -0400

    Significantly changed UI design

--- asset_management_app/routes/home.js
+++ asset_management_app/routes/home.js
@@ -12,156 +12,68 @@ var pool  = mysql.createPool({
     database        : 'asset_management'
 });
 
-exports.main_display = function(req, res){
-    pool.query('SELECT * FROM asset_management.asset_list', function (error, results, fields) {
-        if (error) throw error;
-        var s = [];
-        for (i = 0; i < results.length; i++) {
-            var type_id = results[i].type_id;
-            var asset_type;
-            if (type_id == 1) {  // better solution is to pool.query and join the asset and type tables using asynchronous functions but not sure how
-                asset_type = "Computer Server";
-            }
-            else if (type_id == 2) {
-                asset_type = "Desktop";
-            }
-            else if (type_id == 3) {
-                asset_type = "Laptop";
-            }
-            else {
-                asset_type = "Biotech Machine";
-            }
-            s.push({id: results[i].asset_id, name: results[i].asset_name, type: asset_type, purchase_date: results[i].purchase_date, purchase_price: results[i].purchase_price.toFixed(2), 
-                manu: results[i].manufacturer, s_expire: results[i].support_expiration, annual_s_cost: results[i].annual_support_cost, dep_sched: results[i].depreciation_schedule, 
-                dep_amount: results[i].depreciated_amount, res_val: results[i].residual_value, firmware_lvl: results[i].firmware_level, os_type: results[i].os_type, os_ver: results[i].os_version,
-                s_con: results[i].support_contact, dep: results[i].department, sal_name: results[i].salution_name, serial: results[i].serial_number, internal_con: results[i].internal_contact});
-        }
-        var info = {
-            // authenticated: isAuthed,
-            asset: s
-        };
-        res.render("main_display", info);
-    });
-};
-
-exports.category_search_result = function(req, res){
-    type_id = parseInt(req.query.type_id);
-    pool.query('SELECT * FROM asset_management.asset_list WHERE type_id = ?', type_id, function (error, results, fields) {
-        if (error) throw error;
-        var s = [];
-        for (i = 0; i < results.length; i++) {
-            if (type_id == 1) {  // better solution is to pool.query and join the asset and type tables using asynchronous functions but not sure how
-                asset_type = "Computer Server";
-            }
-            else if (type_id == 2) {
-                asset_type = "Desktop";
-            }
-            else if (type_id == 3) {
-                asset_type = "Laptop";
-            }
-            else {
-                asset_type = "Biotech Machine";
-            }
-            s.push({id: results[i].asset_id, name: results[i].asset_name, type: asset_type, purchase_date: results[i].purchase_date, purchase_price: "$"+results[i].purchase_price.toFixed(2)});
-        }
-        var info = {
-            asset: s
-        };
-        res.render("search_result", info);
-    });
-};
-
-function get_name(req, res, next) {
-    asset_name = req.query.name;
-    res.locals.name_array = [];
-    if (asset_name) {
-        pool.query('SELECT * FROM asset_management.asset_list WHERE asset_name = ?', asset_name, function (error, results, fields) {
-            if (error) throw error;
-            for (i = 0; i < results.length; i++) {
-                res.locals.name_array.push(results[i].asset_id);
-            }
-            next();
-        });
-    }
-    else {
-        next();
-    }
+exports.redir_search = function(req, res) {
+    res.redirect('/search')
 }
 
-function get_type(req, res, next) {
-    type_id = parseInt(req.query.type);
-    res.locals.type_array = [];
-    if (type_id) {
-        pool.query('SELECT * FROM asset_management.asset_list WHERE type_id = ?', type_id, function (error, results, fields) {
-            if (error) throw error;
-            for (i = 0; i < results.length; i++) {
-                res.locals.type_array.push(results[i].asset_id);
-            }
-            next();
-        });
-    }
-    else {
-        next();
-    }
-}
+// exports.main_display = function(req, res){
+//     pool.query('SELECT * FROM asset_management.asset_list', function (error, results, fields) {
+//         if (error) throw error;
+//         var s = [];
+//         for (i = 0; i < results.length; i++) {
+//             var type_id = results[i].type_id;
+//             var asset_type;
+//             if (type_id == 1) {  // better solution is to pool.query and join the asset and type tables using asynchronous functions but not sure how
+//                 asset_type = "Computer Server";
+//             }
+//             else if (type_id == 2) {
+//                 asset_type = "Desktop";
+//             }
+//             else if (type_id == 3) {
+//                 asset_type = "Laptop";
+//             }
+//             else {
+//                 asset_type = "Biotech Machine";
+//             }
+//             s.push({id: results[i].asset_id, name: results[i].asset_name, type: asset_type, purchase_date: results[i].purchase_date, purchase_price: results[i].purchase_price.toFixed(2), 
+//                 manu: results[i].manufacturer, s_expire: results[i].support_expiration, annual_s_cost: results[i].annual_support_cost, dep_sched: results[i].depreciation_schedule, 
+//                 dep_amount: results[i].depreciated_amount, res_val: results[i].residual_value, firmware_lvl: results[i].firmware_level, os_type: results[i].os_type, os_ver: results[i].os_version,
+//                 s_con: results[i].support_contact, dep: results[i].department, sal_name: results[i].salution_name, serial: results[i].serial_number, internal_con: results[i].internal_contact});
+//         }
+//         var info = {
+//             // authenticated: isAuthed,
+//             asset: s
+//         };
+//         res.render("main_display", info);
+//     });
+// };
 
-function get_manu(req, res, next) {
-    manu = req.query.manu;
-    res.locals.manu_array = [];
-    if (manu) {
-        pool.query('SELECT * FROM asset_management.asset_list WHERE manufacturer = ?', manu, function (error, results, fields) {
-            if (error) throw error;
-            for (i = 0; i < results.length; i++) {
-                res.locals.manu_array.push(results[i].asset_id);
-            }
-            next();
-        });
-    }
-    else {
-        next();
-    }
-}
-
-function get_price(req, res, next) {
-    var r1_start = 0;
-    var r2_start = 500;
-    var r3_start = 1000;
-    price_range = parseInt(req.query.price_range);
-    res.locals.price_array = [];
-    if (price_range) {
-        query_str = 'SELECT * FROM asset_management.asset_list WHERE purchase_price BETWEEN ? and ?';
-        if (price_range = 1) {
-            pool.query(query_str, [r1_start, r2_start], function (error, results, fields) {
-                if (error) throw error;
-                for (i = 0; i < results.length; i++) {
-                    res.locals.price_array.push(results[i].asset_id);
-                }
-                next();
-            });
-        }
-        else if (price_range = 2) {
-            pool.query(query_str, [r2_start, r3_start], function (error, results, fields) {
-                if (error) throw error;
-                for (i = 0; i < results.length; i++) {
-                    res.locals.price_array.push(results[i].asset_id);
-                }
-                next();
-            });  
-        }
-        else if (price_range = 3) {
-            pool.query('SELECT * FROM asset_management.asset_list WHERE purchase_price >= ?', r3_start, function (error, results, fields) {
-                if (error) throw error;
-                for (i = 0; i < results.length; i++) {
-                    res.locals.price_array.push(results[i].asset_id);
-                }
-                next();
-            });
-        }
-    }
-    else {
-        next();
-    }
-}
+// exports.category_search_result = function(req, res){
+//     type_id = parseInt(req.query.type_id);
+//     pool.query('SELECT * FROM asset_management.asset_list WHERE type_id = ?', type_id, function (error, results, fields) {
+//         if (error) throw error;
+//         var s = [];
+//         for (i = 0; i < results.length; i++) {
+//             if (type_id == 1) {  // better solution is to pool.query and join the asset and type tables using asynchronous functions but not sure how
+//                 asset_type = "Computer Server";
+//             }
+//             else if (type_id == 2) {
+//                 asset_type = "Desktop";
+//             }
+//             else if (type_id == 3) {
+//                 asset_type = "Laptop";
+//             }
+//             else {
+//                 asset_type = "Biotech Machine";
+//             }
+//             s.push({id: results[i].asset_id, name: results[i].asset_name, type: asset_type, purchase_date: results[i].purchase_date, purchase_price: "$"+results[i].purchase_price.toFixed(2)});
+//         }
+//         var info = {
+//             asset: s
+//         };
+//         res.render("search_result", info);
+//     });
+// };
 
 function get_all_ids(req, res, next) {
     pool.query('SELECT * FROM asset_management.asset_list', function (error, results, fields) {
@@ -173,81 +85,11 @@ function get_all_ids(req, res, next) {
     });
 }
 
-exports.advanced_search_result = [get_name, get_type, get_manu, get_price, get_all_ids, function(req, res) {
-    // console.log(res.locals.name_array.length);
-    // console.log(res.locals.type_array.length);
-    // console.log("");
-    if (res.locals.name_array.length + res.locals.type_array.length + res.locals.manu_array.length + res.locals.price_array.length > 0) {
-        if (!res.locals.name_array.length) {
-            res.locals.name_array = res.locals.all_ids;
-        }
-        if (!res.locals.type_array.length) {
-            res.locals.type_array = res.locals.all_ids;
-        }
-        if (!res.locals.manu_array.length) {
-            res.locals.manu_array = res.locals.all_ids;
-        }
-        if (!res.locals.price_array.length) {
-            res.locals.price_array = res.locals.all_ids;
-        }
-        var id_list = _.intersection(res.locals.name_array, res.locals.type_array, res.locals.manu_array, res.locals.price_array);
-        if (!id_list.length) {
-            res.render("search_fail_msg");
-        }
-        var results_list = [];
-        for (n = 0; n < id_list.length; n++) {
-            id = id_list[n]
-            pool.query('SELECT * FROM asset_management.asset_list WHERE asset_id = ?', id, function (error, results, fields) {
-                if (error) throw error;
-                for (i = 0; i < results.length; i++) {
-                    var type_id = results[i].type_id;
-                    if (type_id == 1) {  // better solution is to pool.query and join the asset and type tables using asynchronous functions but not sure how
-                        asset_type = "Computer Server";
-                    }
-                    else if (type_id == 2) {
-                        asset_type = "Desktop";
-                    }
-                    else if (type_id == 3) {
-                        asset_type = "Laptop";
-                    }
-                    else {
-                        asset_type = "Biotech Machine";
-                    }
-                    results_list.push({id: results[i].asset_id, name: results[i].asset_name, type: asset_type, purchase_date: results[i].purchase_date, purchase_price: "$"+results[i].purchase_price.toFixed(2)});
-                }
-                if (results_list.length == id_list.length) {
-                    var info = {
-                        asset: results_list
-                    };
-                    res.render("search_result", info);
-                }
-            });
-        }
-    }
-    else {
-        res.redirect('/');
-    }
-}]
-
 exports.edit_form = function(req, res) {
     var id = parseInt(req.query.id);
     pool.query('SELECT * FROM asset_management.asset_list WHERE asset_id = ?', id, function (error, results, fields) {
         if (error) throw error;
         var s = [];
-        // var type_id = results[0].type_id;
-        // var asset_type;
-        // if (type_id == 1) {  // better solution is to pool.query and join the asset and type tables using asynchronous functions but not sure how
-        //     asset_type = "Computer Server";
-        // }
-        // else if (type_id == 2) {
-        //     asset_type = "Desktop";
-        // }
-        // else if (type_id == 3) {
-        //     asset_type = "Laptop";
-        // }
-        // else {
-        //     asset_type = "Biotech Machine";
-        // }
         s.push({id: results[0].asset_id, name: results[0].asset_name, type: results[0].type_id, purchase_date: results[0].purchase_date, purchase_price: results[0].purchase_price.toFixed(2), 
         manu: results[0].manufacturer, s_expire: results[0].support_expiration, annual_s_cost: results[0].annual_support_cost, dep_sched: results[0].depreciation_schedule, 
         dep_amount: results[0].depreciated_amount, res_val: results[0].residual_value, firmware_lvl: results[0].firmware_level, os_type: results[0].os_type, os_ver: results[0].os_version,
--- asset_management_app/routes/index.js
+++ asset_management_app/routes/index.js
@@ -1,9 +1,11 @@
+var search_form = require('./search_form.js');
 var home = require('./home.js');
 
 exports.do_set = function(app) {
-    app.get('/', home.main_display);
-    app.get('/category_search_result', home.category_search_result);
-    app.get('/advanced_search_result', home.advanced_search_result);
+    app.get('/', home.redir_search);
+    app.get('/search', search_form.form_display);
+    // app.get('/category_search_result', home.category_search_result);
+    app.get('/advanced_search_result', search_form.advanced_search_result);
     app.get('/edit_form', home.edit_form);
     app.get('/edit_result', home.edit_result);
     app.get('/add_new', home.add_new);
